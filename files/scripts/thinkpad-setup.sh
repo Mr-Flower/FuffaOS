32f212a7f63c2a0f0c8e1357e810e8692cc09d83
Mr-Flower <sciurenz95@gmail.com>
2026-03-27 15:50:31 +0100

    fixes

--- files/scripts/thinkpad-setup.sh
+++ files/scripts/thinkpad-setup.sh
@@ -6,8 +6,5 @@ echo "Configurazione ThinkPad in corso..."
 # Abilita il servizio per le impronte digitali
 systemctl enable fprintd.service
 
-# Abilita TLP per la batteria (ottimo per i ThinkPad)
-systemctl enable tlp.service
-
 # Configurazione opzionale per i tasti funzione o altro hardware specifico
 # Se il tuo ThinkPad ha il trackpoint, solitamente è già supportato dal kernel di Bazzite.
